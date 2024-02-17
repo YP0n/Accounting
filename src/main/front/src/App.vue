6818101955be1783e83a9a6034d169de6dc67feb
Feoktistiv Yuriy <y.feoktistov@mobidev.biz>
2024-02-17 17:58:58 +0200

    Added SpendingPage.vue

---
+++ src/main/front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <spending-page/>
+</template>
+
+<script>
+import SpendingPage from './components/SpendingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SpendingPage
+  }
+}
+</script>
+
+<style>
+
+</style>
