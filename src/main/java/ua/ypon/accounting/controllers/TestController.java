f1641c0d6bbb7ba28058e44bbf89da018900755c
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2025-05-19 13:20:43 +0300

    Update TestController.java

--- src/main/java/ua/ypon/accounting/controllers/TestController.java
+++ src/main/java/ua/ypon/accounting/controllers/TestController.java
@@ -2,13 +2,16 @@ package ua.ypon.accounting.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class TestController {
-    @GetMapping("/hello")
+    @GetMapping
     public String hello() {
         return "Hello from server!";
     }
 }
+
 
